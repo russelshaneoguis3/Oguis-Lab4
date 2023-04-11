0998f43e328393cbf0c2ac3e93af6abdadb19b6b
russelhaneoguis3 <you@example.com>
2023-04-11 21:51:09 +0800

    Second Commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,3 +21,20 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 console.log(app)
+
+const googleSignInBtn = document.querySelector('.google-sign-in');
+const provider = new GoogleAuthProvider();
+const auth = getAuth(app);
+
+googleSignInBtn.addEventListener('click', () => {
+signInWithPopup(auth, provider).then((result) => {
+  const user = result.user;
+  alert(` Hello ${user.displayName}!`);
+  window.location.href = "home.html";
+
+}).catch((error) => {
+  const errorMessage = error.message;
+  alert(`Can't Login: ${errorMessage}`)
+});
+
+});
